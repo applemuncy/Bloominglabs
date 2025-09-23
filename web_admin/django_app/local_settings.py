79fa01685e465bc2796a5c12392a464cd8234f88
apple muncy <applemuncy@users.noreply.github.com>
2025-09-23 15:35:38 -0400

    empty local_settings.py

--- web_admin/django_app/local_settings.py
+++ web_admin/django_app/local_settings.py
@@ -1,18 +1,1 @@
-"""
-
-Github was getting to be a godawful mess w/ the location specific or sensitive stuff.
-So, this.
-
-11/24/2013 SDC
-
-"""
-
-DEBUG = True
-
-RFID_PORT = 6666
-RFID_HOST = 'poquito3.local'
-RFID_PASSWORD = 'xyzzy'
-DATABASE_NAME = '/home/apple/Bloominglabs/BloomingLabs.db'
-PROJECT_ROOT = '/home/apple/Bloominglabs/web_admin'
-ARDUINO_URL = "192.168.1.3"
 
